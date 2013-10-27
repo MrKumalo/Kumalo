cf60e4fea9bcacb55772028b16557bd36c2c637d
solberg <solberg.bjarki@gmail.com>
2013-10-27 22:28:55 +0000

    Bætti við test tilviki

--- TTT/TTT.java
+++ TTT/TTT.java
@@ -2,6 +2,17 @@ public class TTT
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("FooBar");		
+        System.out.println("Welcom to Tic Tac Toe");
+
+        Board board = new Board();
+		System.out.println(board);
+
+
+        board.setCell(2,'x');
+        System.out.println(board);
+
+        board.setCell(2,'o');
+        System.out.println(board);
+
 	}	
 }
