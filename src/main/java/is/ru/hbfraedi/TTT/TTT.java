d38af4a997267990c7da770958b50341308be975
arnarthorsveinsson <arnars12@ru.is>
2013-10-28 14:35:06 +0000

    Shit got real

---
+++ src/main/java/is/ru/hbfraedi/TTT/TTT.java
@@ -0,0 +1,33 @@
+public class TTT 
+{
+	public static void main(String[] args) 
+	{
+
+        System.out.println("Welcom to Tic Tac Toe");
+
+        Board board = new Board();
+		System.out.println(board);
+
+
+        board.setCell(2,'x');
+        System.out.println(board);
+
+        board.setCell(2,'o');
+        System.out.println(board);
+
+
+		Player arnar = null;
+		Player solberg = null;
+		try
+		{
+			arnar = new Player('x', "Arnar");
+			solberg = new Player('o', "Solberg");
+		}
+		catch(XOException e)
+		{
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		Board game = new Board();
+	}	
+}
