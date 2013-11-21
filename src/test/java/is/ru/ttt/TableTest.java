8e199afc8f8455ef2b9bec37698faa6e8dd4daab
Agnarsson <gunnarsa12@ru.is>
2013-11-21 14:19:54 +0000

    Gunnar: buid ad unit-testa json

--- src/test/java/is/ru/ttt/TableTest.java
+++ src/test/java/is/ru/ttt/TableTest.java
@@ -54,4 +54,72 @@ public class TableTest {
     	t.setX('X', 7);
       assertArrayEquals(test,t.getFullTable());
     }
+
+    @Test
+    public void toJsonTest()
+    {
+      Table t = new Table();
+
+      for (int i = 0; i < 9; i++) 
+      {
+        if(i == 1)
+        {
+          t.setX('X', i);
+        } 
+        else
+        {
+          t.setX('E', i);
+        }
+      }
+      String jsonTest = "{\"cell0\":\"E\"},{\"cell1\":\"X\"},{\"cell2\":\"E\"},{\"cell3\":\"E\"},{\"cell4\":\"E\"},{\"cell5\":\"E\"},{\"cell6\":\"E\"},{\"cell7\":\"E\"},{\"cell8\":\"E\"}";
+      assertEquals(jsonTest, t.toJson());
+    }
+
+    @Test
+    public void toJsonFullTest()
+    {
+      Table t = new Table();
+
+      for (int i = 0; i < 9; i++) 
+      {
+        if(i == 1)
+        {
+          t.setX('X', i);
+        } 
+        else if(i == 2)
+        {
+          t.setX('O', i);
+        }
+        else
+        {
+          t.setX('E', i);
+        }
+      }
+      String jsonTest = "{\"cell0\":\"E\"},{\"cell1\":\"X\"},{\"cell2\":\"O\"},{\"cell3\":\"E\"},{\"cell4\":\"E\"},{\"cell5\":\"E\"},{\"cell6\":\"E\"},{\"cell7\":\"E\"},{\"cell8\":\"E\"}";
+      assertEquals(jsonTest, t.toJson());
+    }
+
+    @Test
+    public void toJsonExtraTest()
+    {
+      Table t = new Table();
+
+      for (int i = 0; i < 9; i++) 
+      {
+        if(i == 0 || i == 4 || i == 8)
+        {
+          t.setX('X', i);
+        } 
+        else if(i == 2 || i == 5 || i == 6)
+        {
+          t.setX('O', i);
+        }
+        else
+        {
+          t.setX('E', i);
+        }
+      }
+      String jsonTest = "{\"cell0\":\"X\"},{\"cell1\":\"E\"},{\"cell2\":\"O\"},{\"cell3\":\"E\"},{\"cell4\":\"X\"},{\"cell5\":\"O\"},{\"cell6\":\"O\"},{\"cell7\":\"E\"},{\"cell8\":\"X\"}";
+      assertEquals(jsonTest, t.toJson());
+    }
 }
