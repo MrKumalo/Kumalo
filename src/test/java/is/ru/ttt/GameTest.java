cd65e1505186ecf51ddc2fe9972ba71d977969a4
solberg <solberg.bjarki@gmail.com>
2013-11-21 15:57:42 +0000

    Solberg: game test

--- src/test/java/is/ru/ttt/GameTest.java
+++ src/test/java/is/ru/ttt/GameTest.java
@@ -15,19 +15,21 @@ public class GameTest
 		Player player1 = new Player("p1", true);
 		Player player2 = new Player("p2", false);
 		Table t = new Table();
+		
+		Game game = null;
 		try
 		{
-			Game game = new Game(player1, player2, t, 1);
+			game = new Game(player1, player2, t, 1);
 		}
 		catch(IllegalTurnException e)
 		{
 			System.out.println("Svona a thetta ekki ad vera!");
 		}
 
-		System.out.println(game.getP1().toJson());
+		//System.out.println(game.getP1().toJson());
 		//isEquals(player1, p1);
 		//assertEquals(player2, game.getP2());
 		//assertEquals(t, game.getTable());
-		//assertEquals(1, game.getTurn());
+		assertEquals(1, game.getTurn());
 	}
 }
