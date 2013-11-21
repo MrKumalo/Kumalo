7c1a8352dc6410e2fa5f882850d1173c8cf1ab93
solberg <solberg.bjarki@gmail.com>
2013-11-21 20:55:34 +0000

    Solberg: javadoc

--- src/main/java/is/ru/ttt/AlreadyOccupiedException.java
+++ src/main/java/is/ru/ttt/AlreadyOccupiedException.java
@@ -6,14 +6,20 @@
 package is.ru.ttt;
 
 /**
- * [lýsing]
+ * 
  *
  * @author arnars12, axelg12, gadidjah12, gunnarsa12, haraldurs12, solberg12  
  * @version 1, 20 November 2013
  */
-public class AlreadyOccupiedException extends Exception {
-
-    public AlreadyOccupiedException(String message) {
+public class AlreadyOccupiedException extends Exception 
+{
+	/**
+	 * [AlreadyOccupiedException description]
+	 * @param  message [description]
+	 * @return         [description]
+	 */
+    public AlreadyOccupiedException(String message) 
+    {
         
         super(message);
     }
