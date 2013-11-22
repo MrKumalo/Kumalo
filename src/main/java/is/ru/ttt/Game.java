b490491b0fca805228a9579c5d501b88c1ee3c2c
Agnarsson <gunnarsa12@ru.is>
2013-11-22 15:48:59 +0000

    gunnar: buinn ad commenta game.java....gamligamli

--- src/main/java/is/ru/ttt/Game.java
+++ src/main/java/is/ru/ttt/Game.java
@@ -21,7 +21,7 @@ public class Game {
 	private int turnCounter;
 
 	/**
-	 *	
+	 *	This constructor takes in 4 variables and initializes them.
 	 *
 	 * @param	p1 						Intance of player.
  	 * @param	p2 						Intance of player.
@@ -42,6 +42,9 @@ public class Game {
 		this.turn = turn;
 	}
 
+	/**
+	 *	This is the default constructor, which initializes the variables.
+	 */
 	public Game()
 	{
 		p1 = null;
@@ -49,52 +52,101 @@ public class Game {
 		table = null;
 		turn = -1;
 	}
+
+	/**
+	 * The setCounter function changes the counter variable which is used to determine if the game is a draw.
+	 * @param	counter 	This variable counts the turns already played.
+	 */
 	public void setCounter(int counter)
 	{
 		turnCounter = 0;
-	}
+	} 
+
+	/**
+	 * The getTurnCounter function returns the turnCounter which holds the info on turns played.
+	 * @return 	turnCounter variable.
+	 */
 	public int getTurnCounter()
 	{
 		return turnCounter;
 	}
+
+	/**
+	 * The getP1 function returns player1.
+	 * @return 	player1
+	 */
 	public Player getP1()
 	{
 		return this.p1;
 	}
 
+	/**
+	 * The getP1 function returns player2.
+	 * @return 	player2.
+	 */
 	public Player getP2()
 	{
 		return this.p2;
 	}
+
+	/**
+	 * The addCounter function increases the counter variable by 1.
+	 */
 	public void addCounter()
 	{
 		turnCounter++;
 	}
+
+	/**
+	 * The getTable function returns the current gametable.
+	 * @return 	table.
+	 */
 	public Table getTable()
 	{
 		return this.table;
 	}
 
+	/**
+	 * The getTurn function returns the current turn.
+	 * @return 	turn.
+	 */
 	public int getTurn()
 	{
 		return this.turn;
 	}
 
+	/**
+	 * The setP1 function sets player1 as p1 in the game.
+	 * @param p1 	player1 intance
+	 */
 	public void setP1(Player p1)
 	{
 		this.p1 = p1;
 	}
 
+	/**
+	 * The setP2 function sets player2 as p2 in the game.
+	 * @param p2 	player2 intance
+	 */
 	public void setP2(Player p2)
 	{
 		this.p2 = p2;
 	}
 
+	/**
+	 * The setTable function sets table as current table.
+	 * @param table 	table instance
+	 */
 	public void setTable(Table table)
 	{
 		this.table = table;
-	}	
+	}
 
+	/**
+	 * The setTurn function sets the current turn.
+	 * @param turn 	current turn
+	 * @throws IllegalTurnException 	This exception is thrown if turn is not 1 or 2.
+	 */	
 	public void setTurn(int turn) throws IllegalTurnException
 	{
 		if(turn != 1 && turn != 2)
@@ -103,6 +155,11 @@ public class Game {
 		this.turn = turn;
 	}
 
+	/**
+	 * The winningCombinations function is a boolean function which defines how a player can win the game.
+	 * @return boolean true.
+	 * @param 	cells 		Arraylist for cells combinations that can win the game.
+	 */
 	public Boolean winningCombinations(ArrayList<Integer> cells)
 	{
 		if(cells.contains(0) && cells.contains(1) && cells.contains(2))
@@ -132,6 +189,10 @@ public class Game {
 		return false;
 	}
 
+	/**
+	 * The toJson function creates a json string and returns it.
+	 * @return json string
+	 */
 	public String toJson()
 	{
 		String json = "[";
